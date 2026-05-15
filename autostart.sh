b3314385b24f1b5965c898d49b6dbfa872278aad
guzlo133767 <komirnyjmisa40@gmail.com>
2026-05-15 09:23:01 +0300

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+
+feh --bg-fill ~/Documents/cuh.jpg & /usr/libexec/polkit-gnome-authentication-agent-1 & setxkbmap -layout us,ru -option grp:alt_shift_toggle & picom --backend glx --daemon & wal - R & playerctld daemon & dwmblocks & disown 
