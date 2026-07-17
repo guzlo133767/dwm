1718ad5c4a807f5c7d861a87b508bf001d3ad966
guzlo133767 <komirnyjmisa40@gmail.com>
2026-07-17 13:09:39 +0300

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -6,22 +6,23 @@ static const unsigned int snap      = 16;       /* snap pixel */
 static const unsigned int gappx     = 3;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=12" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#666666";
 static const char col_gray3[]       = "#ffffff";
 static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#000000";
-static const char col_cyan2[] 	    = "#888888";
+static const char col_cyan2[] 	    = "#ffffff";
+static const char col_blue1[]	    = "#888888";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan2, col_cyan2 },
+	[SchemeSel]  = { col_gray4, col_blue1, col_cyan2 },
 };
 
 /* tagging */
-static const char *tags[] = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+static const char *tags[] = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -62,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "rofi","-show","drun", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *slockcmd[] = { "slock", NULL };
 /*static const char *volup[] = { "wpctl", "set-volume", "57", "5%+", NULL }
